77b631cd3aa379d5fdbae694fb4d85fe2ea55ef3
Joel Valencia <trekkerj1@gmail.com>
2016-03-17 15:37:54 -0500

    setup getAllPosts and getSinglePost factories

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,19 +2,22 @@
 
 var controllers = angular.module('myBlogApp.controllers', []);
 
-controllers.controller('listPostsController', ['$scope', 'myFactory', function($scope, myFactory) {
+controllers.controller('listPostsController', ['$scope', 'myFactory', function ($scope, myFactory) {
     console.log("test");
-    
     myFactory.test();
     
+    $scope.posts = myFactory.getAllPosts();
+  
 }]);
 
-controllers.controller('singlePostController', ['$scope', function($scope) {
-       
+controllers.controller('singlePostController', ['$scope', 'myFactory', '$routeParams', function ($scope, myFactory, $routeParams) {
+    var id = $routeParams.id;
+    
+    $scope.post = myFactory.getSinglePost(id);
     
 }]);
 
-controllers.controller('createPostController', ['$scope', function($scope) {
+controllers.controller('createPostController', ['$scope', function ($scope) {
     
     
 }]);
--- client/js/factories.js
+++ client/js/factories.js
@@ -1,12 +1,30 @@
 //The action to call the local API should be here
 
-var app = angular.module('myBlogApp.factories', []);
+var app = angular.module('myBlogApp.factories', ['ngResource']);
 
-app.factory('myFactory', ['$http', function($http) {
+app.factory('myFactory', ['$resource', function($resource) {
     var f = {};
     
     f.test = function() {
         console.log("factory works");
+    }
+    
+    f.getAllPosts = function() {
+        var resource = $resource('/api/posts/:id');
+        return resource.query(function(data) {
+            return data;
+        });
+        
+    }
+    
+    f.getSinglePost = function(id) {
+        var resource = $resource('/api/posts/:id');
+        var param = {};
+        param.id = id;
+        return resource.get(param, function(data) {
+            return data;
+        });
+        
     }
     
     return f;
